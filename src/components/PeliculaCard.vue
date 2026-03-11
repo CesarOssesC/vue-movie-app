d75619c58dd950e42ce13b9c5c681c7e2c1821e1
César Osses <cesar.osses@gmail.com>
2026-03-11 18:03:24 -0300

    agregando crud peliculas

--- src/components/PeliculaCard.vue
+++ src/components/PeliculaCard.vue
@@ -14,7 +14,7 @@
             </p>
         </div>
         <div class="card-footer text-center">
-            <button class="btn btn-info btn-sm me-2">Detalles</button>
+            <router-link :to="`/peliculas/${pelicula.id}`" class="btn btn-info btn-sm me-2">Detalles</router-link>
             <button class="btn btn-primary btn-sm me-2">Editar</button>
             <button class="btn btn-danger btn-sm">Eliminar</button>
         </div>
